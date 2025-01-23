45fb178477ebad257ab78de1bd2c5a2e6f67c689
xotsu <xotsuj@gmail.com>
2025-01-23 12:44:59 +0000

    feat(auth): Finalises doStoredAuth and settupAuth

--- src/auth.ts
+++ src/auth.ts
@@ -12,19 +12,23 @@ export class DropboxAuthService{
   private dropboxAuth: DropboxAuth;
   private accessTokenStore: AccessTokenStore | null;
   private dropbox: Dropbox;
+  private redirectUri: string;
+  private authUrl: string;
+  private dropboxCodeVerifier: string;
 
-  constructor(accessTokenStore: AccessTokenStore | null){
+  constructor(accessTokenStore: AccessTokenStore | null, redirectUri: string){
     this.accessTokenStore = accessTokenStore;
+    this.redirectUri = redirectUri;
   }
 
   public async function attemptAuth(){
     try {
       if (this.accessTokenStore){
         console.log("ObsidianCloud: Attempting stored auth")
-        await doStoredAuth(accessTokenStore);
+        await this.doStoredAuth(accessTokenStore);
       } else {
         console.log("ObsidianCloud: Attempting auth setup")
-        await setupAuth();
+        await this.setupAuth();
       }
       
     } catch (error) {
@@ -47,27 +51,17 @@ export class DropboxAuthService{
     this.dropbox = new Dropbox({
       auth: this.dropboxAuth,
     })
-
   }
 
-  public async function authenticateLocally(dropboxAuth: DropboxAuth){
-    dropboxAuth.access_token = "" 
-    dropboxAuth.refresh_token = ""
-
-    await dropboxAuth.checkAndRefreshAccessToken()
-
-    return new Dropbox({
-      auth: dropboxAuth
+  public async function setupAuth(): Promise<void> {
+    this.dropboxAuth = new DropboxAuth({
+      clientId: DROPBOX_APP_KEY,
     })
-  }
 
-  public async function authenticateWithDropbox(dropboxAuth: DropboxAuth, redirectUri: string) : string {
-
-    // const authUrl = `https://www.dropbox.com/oauth2/authorize?client_id=${DROPBOX_APP_KEY}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=token`
-    
-    const authUrl = String(
-      await dropboxAuth.getAuthenticationUrl(
-        redirectUri,
+    // this.authUrl = `https://www.dropbox.com/oauth2/authorize?client_id=${DROPBOX_APP_KEY}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=token`
+    this.authUrl = String(
+      await this.dropboxAuth.getAuthenticationUrl(
+        this.redirectUri,
         undefined,
         "code",
         "offline",
@@ -77,7 +71,10 @@ export class DropboxAuthService{
       )
     )
 
-    window.location.assign(authUrl)
+    this.dropboxCodeVerifier = this.dropboxAuth.getCodeVerifier();
+
+    window.location.assign(this.authUrl)
+
   }
 
   public async function doAuth(dropboxAuth: DropboxAuth, redirectUri: string, params: any){
@@ -103,6 +100,55 @@ export class DropboxAuthService{
     })
 
   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  public async function authenticateLocally(dropboxAuth: DropboxAuth){
+    dropboxAuth.access_token = "" 
+    dropboxAuth.refresh_token = ""
+
+    await dropboxAuth.checkAndRefreshAccessToken()
+
+    return new Dropbox({
+      auth: dropboxAuth
+    })
+  }
+
+  public async function authenticateWithDropbox(dropboxAuth: DropboxAuth, redirectUri: string) : string {
+
+    
+    const authUrl = String(
+      await dropboxAuth.getAuthenticationUrl(
+        redirectUri,
+        undefined,
+        "code",
+        "offline",
+        undefined,
+        undefined,
+        true
+      )
+    )
+
+    window.location.assign(authUrl)
+  }
+
 
 
   public function handleRedirectForToken(): string | null {
